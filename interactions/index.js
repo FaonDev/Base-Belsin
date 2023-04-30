ce06622194d6eca6366558ee116c88c3e1bd8510
FaonDev <124811747+FaonDev@users.noreply.github.com>
2023-04-30 01:43:36 -0300

    Tiny update

--- interactions/index.js
+++ interactions/index.js
@@ -6,6 +6,5 @@ const ApplicationCommands = []
 for (const event of readdirSync(`${__dirname}/events`)) discordApp.on(require(`./events/${event}`).name, (...params) => require(`./events/${event}`).execute(...params))
 for (const command of readdirSync(`${__dirname}/commands`)) ApplicationCommands.push(require(`./commands/${command}`))
 
-
 discordApp.on('interactionCreate', interaction => interaction.isChatInputCommand() && ApplicationCommands.find(command => command.name === interaction.commandName).execute(interaction))
 discordApp.on('ready', () => discordApp.application.commands.set(ApplicationCommands))
